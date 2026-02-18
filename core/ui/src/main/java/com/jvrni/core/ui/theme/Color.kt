5f237b6f87d3aea3b801b5041c93e5bbfb7efbf8
Jvrni <joao.varani@gmail.com>
2026-02-18 10:22:59 -0300

    Created theme, colors and tipography

---
+++ core/ui/src/main/java/com/jvrni/core/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.jvrni.core.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF0074e6)
+val Secondary = Color(0xFFebeef1)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
---
+++ core/ui/src/main/java/com/jvrni/core/ui/theme/Theme.kt
@@ -0,0 +1,40 @@
+package com.jvrni.core.ui.theme
+
+import android.app.Activity
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+
+private val LightColorScheme = lightColorScheme(
+    primary = Primary,
+    secondary = Secondary,
+    tertiary = White,
+    onTertiary = Black
+)
+
+@Composable
+fun NewsAppTheme(
+    content: @Composable () -> Unit
+) {
+    val colorScheme = LightColorScheme
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
